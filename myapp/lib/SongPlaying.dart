637f7743020282e6dd25c1495bf53325c52d241d
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2020-11-07 16:55:35 +0100

    filling songplaying

--- myapp/lib/SongPlaying.dart
+++ myapp/lib/SongPlaying.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,12 +7,36 @@ class SongPlaying extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height:200,
+      height: 180,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.vertical(
           top: Radius.circular(60),
         ),
         color: Color.fromRGBO(132, 218, 165, 1),
+      ),
+      child: Row(
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.black,
+          ),
+          SizedBox(width: 5),
+          Column(
+            children: [
+              Text(
+                "Now Playing",
+                style: TextStyle(),
+              ),
+              Text("Lost in Amsterdam"),
+            ],
+          ),
+          Container(
+            width: 30,
+            height: 30,
+            color: Colors.black,
+          ),
+        ],
       ),
     );
   }
