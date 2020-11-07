40a3a0bf7969d4cc77d273f3875eaeadff0676ea
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2020-11-07 23:45:22 +0100

    cleaning code and solving bugs

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -29,38 +29,46 @@ class MainPage extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             child: SongPlaying(),
           ),
-          Align(
-            alignment: Alignment.topCenter,
-            child: Column(
-              children: [
-                SizedBox(height: 80),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Transform.scale(
-                      scale: 0.8,
-                      child: Icon(
-                        Icons.arrow_back_ios,
-                        color: Colors.grey[700],
-                      ),
-                    ),
-                    SizedBox(width: 40),
-                    Text(
-                      "Liked Songs",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Colors.grey[300],
-                      ),
-                    ),
-                    SizedBox(width: 40),
-                    Icon(
-                      Icons.more_horiz,
-                      color: Colors.grey[300],
-                    ),
-                  ],
+          _UpsideBar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _UpsideBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Column(
+        children: [
+          SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Transform.scale(
+                scale: 0.8,
+                child: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.grey[700],
                 ),
-              ],
-            ),
+              ),
+              SizedBox(width: 40),
+              Text(
+                "Liked Songs",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[300],
+                ),
+              ),
+              SizedBox(width: 40),
+              Icon(
+                Icons.more_horiz,
+                color: Colors.grey[300],
+              ),
+            ],
           ),
         ],
       ),
