791c45f2137b2bf5a8152751c87397b596d9d982
Bernat Casanas <47743689+BernatCasanas@users.noreply.github.com>
2020-11-07 23:38:36 +0100

    done superiopr bar

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -24,28 +24,43 @@ class MainPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(Icons.arrow_back),
-                Text(
-                  "Liked Songs",
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.grey[300],
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                Icon(Icons.more_horiz),
-              ],
-            ),
-          ),
           UserPanel(),
           Align(
             alignment: Alignment.bottomCenter,
             child: SongPlaying(),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Column(
+              children: [
+                SizedBox(height: 80),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Transform.scale(
+                      scale: 0.8,
+                      child: Icon(
+                        Icons.arrow_back_ios,
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text(
+                      "Liked Songs",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey[300],
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Icon(
+                      Icons.more_horiz,
+                      color: Colors.grey[300],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ],
       ),
